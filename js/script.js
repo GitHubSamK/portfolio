237743b4d402b95680dc923295a8cd1f6d8b6681
gitsamk <skrame18@gmail.com>
2024-03-27 19:37:49 -0700

    init

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const skillsSection = document.querySelector('.skills');
+const observer = new IntersectionObserver((appearance) => {
+    appearance.forEach((availability) => {
+        if (availability.isIntersecting) {
+            skillsSection.classList.add('seen');
+        }
+    });
+}, { threshold: 0.5 }); 
+
+observer.observe(skillsSection);
